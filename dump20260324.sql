73fff9558a2d08cc4340d1081ef38a85a6fe06bd
RachelPezzin <rachelpezzin@gmail.com>
2026-03-24 09:09:11 -0300

    v20260324

---
+++ dump20260324.sql
@@ -0,0 +1,82 @@
+CREATE DATABASE  IF NOT EXISTS `controle_temp` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `controle_temp`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: controle_temp
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `controlador`
+--
+
+DROP TABLE IF EXISTS `controlador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `controlador` (
+  `idcontrolador` int NOT NULL AUTO_INCREMENT,
+  `setor` varchar(45) NOT NULL,
+  `setpoint` varchar(45) NOT NULL,
+  PRIMARY KEY (`idcontrolador`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `controlador`
+--
+
+LOCK TABLES `controlador` WRITE;
+/*!40000 ALTER TABLE `controlador` DISABLE KEYS */;
+INSERT INTO `controlador` VALUES (2,'tunel de congelamento','-9,5'),(3,'presuntaria','3,4');
+/*!40000 ALTER TABLE `controlador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `leitura`
+--
+
+DROP TABLE IF EXISTS `leitura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `leitura` (
+  `idleitura` int NOT NULL AUTO_INCREMENT,
+  `TempLida` decimal(5,1) NOT NULL,
+  `DataLeitura` datetime NOT NULL,
+  `controlador_idcontrolador` int NOT NULL,
+  PRIMARY KEY (`idleitura`),
+  KEY `fk_leitura_controlador_idx` (`controlador_idcontrolador`),
+  CONSTRAINT `fk_leitura_controlador` FOREIGN KEY (`controlador_idcontrolador`) REFERENCES `controlador` (`idcontrolador`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `leitura`
+--
+
+LOCK TABLES `leitura` WRITE;
+/*!40000 ALTER TABLE `leitura` DISABLE KEYS */;
+INSERT INTO `leitura` VALUES (1,25.5,'2025-02-12 00:00:00',2);
+/*!40000 ALTER TABLE `leitura` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-24  9:08:50
